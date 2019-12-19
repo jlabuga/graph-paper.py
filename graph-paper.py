79488fe75a72dd803743b32dbefd9660804493d6
Julien Labbé <Julien.Labbe@univ-grenoble-alpes.fr>
2019-12-19 22:26:37 +0100

    Add subgrid for log scale

--- graph-paper.py
+++ graph-paper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 ##############################
 # papier échelles lin et log
@@ -10,7 +11,7 @@ from pylab import *
 
 # paramètres
 # mettre 0 pour avoir une échelle linéaire
-decades_en_x = 0
+decades_en_x = 2
 decades_en_y = 0
 
 # nom fichier sortie
@@ -34,7 +35,6 @@ cm2inch = 1/2.54 # inch per cm
 fig = figure(figsize=(largeur_papier*cm2inch,hauteur_papier*cm2inch))
 ax = fig.add_subplot(111)
 
-# lin ou log
 xmin = 0
 xmax = largeur_figure
 ymin = 0
@@ -45,6 +45,17 @@ if (decades_en_x > 0):
     xmax = xmin*10**decades_en_x
     xscale('log')
     filename += "-"+str(decades_en_x)+"log"
+    ax.xaxis.set_minor_locator(matplotlib.ticker.LogLocator(
+        subs=[1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,
+              2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,
+              3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,
+              4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,
+              5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,
+              6.2,6.4,6.6,6.8,
+              7.2,7.4,7.6,7.8,
+              8.2,8.4,8.6,8.8,
+              9.2,9.4,9.6,9.8]))
+    ax.xaxis.set_major_locator(matplotlib.ticker.LogLocator(subs=[1.0,2,3,4,5,6,7,8,9]))
 else:
     filename += "-lin"
     ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
@@ -55,6 +66,17 @@ if (decades_en_y > 0):
     ymax = ymin*10**decades_en_y
     yscale('log')
     filename += "-"+str(decades_en_y)+"log"
+    ax.yaxis.set_minor_locator(matplotlib.ticker.LogLocator(
+        subs=[1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,
+              2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,
+              3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,
+              4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,
+              5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,
+              6.2,6.4,6.6,6.8,
+              7.2,7.4,7.6,7.8,
+              8.2,8.4,8.6,8.8,
+              9.2,9.4,9.6,9.8]))
+    ax.yaxis.set_major_locator(matplotlib.ticker.LogLocator(subs=[1.0,2,3,4,5,6,7,8,9]))
 else:
     filename += "-lin"
     ax.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
@@ -64,13 +86,21 @@ xlim(xmin,xmax)
 ylim(ymin,ymax)
 
 # label et ticks
-xlabel = "" 
+xlabel = ""
 ylabel = ""
-ax.yaxis.set_ticks_position('none')
 ax.xaxis.set_ticks_position('none')
+ax.yaxis.set_ticks_position('none')
 
-xticks(color='none',fontsize=0)
-yticks(color='none',fontsize=0)
+# ax.xaxis.set_major_formatter(NullFormatter())
+ax.xaxis.set_minor_formatter(NullFormatter())
+ax.xaxis.set_major_formatter(FuncFormatter(lambda value, index: (index)%9+1))
+
+
+# ax.set_xticklabels(["1","2","3","4","5","6","7","8","9"],minor=True, visible=True)
+# ax.set_xticklabels([],minor=True)
+ax.set_yticklabels([])
+# xticks(color='none')
+# yticks(color='none')
 
 # grille
 ax.set_axisbelow(True) # grille sous le tracé
@@ -87,7 +117,7 @@ fig.subplots_adjust(left   = marge_gauche / largeur_papier,
 
 # Sauver
 savefig(filename+extension,dpi=600)
-print("Fichier "+filename+extension+"créé.")
+print("Fichier "+filename+extension+" créé.")
 
 # Show result on screen
 #show()
